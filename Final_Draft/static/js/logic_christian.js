f18448deaf2c69a95154e405fa4aa466564b87d0
Jayce Spiva <jayce.spiva@gmail.com>
2024-07-22 20:57:16 -0700

    final folder maintenance

--- Final_Draft/static/js/logic_christian.js
+++ Final_Draft/static/js/logic_christian.js
@@ -16,7 +16,7 @@ let selectedState = null;
 let pieChart = null;
 
 // Load the CSV data
-d3.csv("Alcohol_Consumption_US.csv").then(function(data) {
+d3.csv("Source_Data/Alcohol_Consumption_US.csv").then(function(data) {
     data.forEach(d => {
         if (!stateData[d.State]) {
             stateData[d.State] = [];
